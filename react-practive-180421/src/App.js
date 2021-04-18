67cc607b4567a391fcda6ab15f97abfa8100b093
github username <github email address>
2021-04-18 13:52:57 +0100

    create app skeleton

--- react-practive-180421/src/App.js
+++ react-practive-180421/src/App.js
@@ -1,7 +1,18 @@
 import './App.css'
 
 function App () {
-  return <div>IS this working?</div>
+  return (
+    <div className='App'>
+      <h1>To-Do App</h1>
+      <form>
+        <input placeholder='enter your to-do here'></input>
+        <button>Add To-Do</button>
+      </form>
+      <ul>
+        <li>to-do</li>
+      </ul>
+    </div>
+  )
 }
 
 export default App
