9da75d70a2f44d8825f63a715cb816d0ed3cfc8a
github username <github email address>
2021-04-18 14:08:33 +0100

    added state and hard-coded to-dos

--- react-practive-180421/src/App.js
+++ react-practive-180421/src/App.js
@@ -1,18 +1,42 @@
 import './App.css'
+import React from 'react'
 
-function App () {
-  return (
-    <div className='App'>
-      <h1>To-Do App</h1>
-      <form>
-        <input placeholder='enter your to-do here'></input>
-        <button>Add To-Do</button>
-      </form>
-      <ul>
-        <li>to-do</li>
-      </ul>
-    </div>
-  )
+class App extends React.Component {
+  state = {
+    formInput: '',
+    todos: [
+      {
+        task: 'sweep all the things',
+        id: 1,
+        completed: false
+      },
+      {
+        task: 'dishes',
+        id: 1,
+        completed: false
+      },
+      {
+        task: 'win at coding',
+        id: 1,
+        completed: false
+      }
+    ]
+  }
+
+  render () {
+    return (
+      <div className='App'>
+        <h1>To-Do App</h1>
+        <form>
+          <input placeholder='enter your to-do here' />
+          <button>Add To-Do</button>
+        </form>
+        <ul>
+          <li>{this.state.todos}</li>
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default App
